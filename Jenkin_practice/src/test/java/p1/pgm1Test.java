bc07bc4f9c90f4ac558f2ff769a3484d7b27ceb3
Neha C Zutti <Neha C Zutti@TYSS-Neha-C-Zutti.mshome.net>
2024-05-21 13:25:56 +0530

    jenkins executed

--- Jenkin_practice/src/test/java/p1/pgm1Test.java
+++ Jenkin_practice/src/test/java/p1/pgm1Test.java
@@ -6,9 +6,9 @@ public class pgm1Test {
 
 	@Test 
 	public void pgmTest() {
-		System.out.println("JENKINS");
+		System.out.println("----JENKINS-----");
 		
-		
+		System.out.println("JENKINS Executed");
 		
 	}
 }
